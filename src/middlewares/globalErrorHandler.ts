4032263f896406e6177f1a41fdec9d73cef3a72e
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-01-10 13:13:38 +0530

    eslint setup

---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,5 @@
+// import { Response, Request, NextFunction } from 'express';
+
+// export default (err: Error, _: Request, res: Response, _: NextFunction) => {
+//   res.status(200).json(err);
+// };
